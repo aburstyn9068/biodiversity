aeac7a915fc73bd4bb8a83bdfc0455853b47b563
Adam Burstyn <adam.burstyn@gmail.com>
2021-05-17 18:02:02 -0400

    updadted code

---
+++ docs/static/js/app.js
@@ -0,0 +1,11 @@
+// Read the JSON file
+d3.json("samples.json").then((data) => {
+    console.log(data);
+    //  Create the Traces
+    // var trace1 = {
+    //   x: data.organ,
+    //   y: data.survival.map(val => Math.sqrt(val)),
+    //   type: "bar",
+    //   name: "Biodiversity",
+    //   orientation: "h"
+});
