2d3d24d976045b8c838ca02baa88c4f8000d531b
Adam Burstyn <adam.burstyn@gmail.com>
2021-05-18 12:06:07 -0400

    updatd drop down menu

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -1,8 +1,21 @@
 // Read the JSON file
 d3.json("samples.json").then((data) => {
     console.log(data);
+    
+    // Get names to update dropdown menu
     var names = data.names;
     console.log(names);
+
+    // Get a reference to the drop down menu
+    var menu = d3.select("#selDataset");
+    
+    names.forEach(function(name) {
+        var item = menu.append("option");
+        var cell = item.append("option");
+        cell.text(name);
+        cell.value(name);
+      });
+
     //  Create the Traces
     // var trace1 = {
     //   x: data.sample.sample_values,
