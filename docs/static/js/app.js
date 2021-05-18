43c7375b8e62e4f633909e7f893338304319b8a2
Adam Burstyn <adam.burstyn@gmail.com>
2021-05-18 13:08:47 -0400

    updatd drop down menu

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -5,17 +5,17 @@ d3.json("samples.json").then((data) => {
     // Get names to update dropdown menu
     var names = data.names;
     console.log(names);
-
     // Get a reference to the drop down menu
     var menu = d3.select("#selDataset");
-    
     // Iterate through names and add each name as an option in the drop down menu
     names.forEach(function(name) {
-        console.log(name);
         var item = menu.append("option");
         item.text(name);
-        //item.value(name);
-      });
+        item.attr("value", name);
+    });
+
+    // Update demographic info
+             
 
     //  Create the Traces
     // var trace1 = {
