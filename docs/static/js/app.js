907b1cd551cc480197779324d10b6f7a9427e625
Adam Burstyn <adam.burstyn@gmail.com>
2021-05-18 11:23:26 -0400

    updated read json

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -1,11 +1,21 @@
 // Read the JSON file
 d3.json("samples.json").then((data) => {
     console.log(data);
+    names = data.names;
+    console.log(names);
     //  Create the Traces
     // var trace1 = {
-    //   x: data.organ,
-    //   y: data.survival.map(val => Math.sqrt(val)),
+    //   x: data.sample.sample_values,
+    //   y: data.sample.out_ids,
     //   type: "bar",
     //   name: "Biodiversity",
     //   orientation: "h"
 });
+
+// Unpack function
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+}
+
